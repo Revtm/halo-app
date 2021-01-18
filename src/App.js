207f56123ab7d1b467803c6a68c890f4e9e4682b
revtm <kykylizaro98@gmail.com>
2021-01-18 22:37:28 +0700

    belajar react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Todo from "./components/Todo";
+import FilterButton from "./components/FilterButton";
+import Form from "./components/Form";
+import { nanoid } from "nanoid";
 
-function App() {
+function App(props) {
+  const [tasks, setTasks] = useState(props.tasks);
+  function addTask(name){
+    const newTask = {id: "todo-"+nanoid(), name: name, completed:false};
+    setTasks([...tasks, newTask]);
+  }
+  function deleteTask(id){
+    const remainingTask = tasks.filter(tasks => id !== tasks.id);
+    setTasks(remainingTask);
+  }
+  const tasksList = tasks.map(task =>
+    <Todo id={task.id} name={task.name} completed={task.completed} key={task.id} deleteTask={deleteTask}/>
+  );
+  const tasksNoun = tasksList.length !== 1 ? 'tasks' : 'task';
+  const headingText = `${tasksList.length} ${tasksNoun} remaining`;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todoapp stack-large">
+      <Form addTask={addTask} />
+      <FilterButton />
+      <FilterButton />
+      <FilterButton />
+      <h2 id="list-heading">
+        {headingText}
+      </h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+      {tasksList}
+      </ul>
     </div>
   );
 }
